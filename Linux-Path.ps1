57f4640b1b94c7ba8fe0f71f927592be6f196444
Nicolai Krohg <nick@nve.no>
2025-11-06 12:44:33 +0100

    init

---
+++ Linux-Path.ps1
@@ -0,0 +1,26 @@
+﻿param(
+    [Parameter(
+        Mandatory=$true,
+        ValueFromPipeline = $true,
+        ValueFromPipelineByPropertyName = $true)]
+    [string]$WindowsPath
+)
+
+process {
+
+# Normalize porential mixed slashes
+    $normalized = $WindowsPath -replace '\\', '/'
+
+# Detect drive letter using regex
+    if ($normalized -match '^([A-Za-z]):/(.*)') {
+        $drive = $matches[1].ToLower()
+        $rest = $matches[2]
+        $wslPath = "/mnt/$drive/$rest"
+    }
+    else {
+        # No drive letter > treat as relative path
+        $wslPath = $normalized
+    }
+
+    Write-Output $wslPath
+}
